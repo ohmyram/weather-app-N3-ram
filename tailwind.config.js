aaadb01e7a36e217ee8c1df932e6edad4f790ca1
ohmyram <ohmyram11@gmail.com>
2024-06-06 12:51:35 -0700

    actualizacion de color de mi fuente, envio para revision

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      customDark: 'rgba(16, 14, 29, 1)',
-      customSecondaryDark: 'rgba(30, 33, 58, 1)',
-      customGray: 'rgba(97, 100, 117, 1)',
-      customgraytwo: 'rgba(110, 112, 122, 1)',
-      customtextW: 'rgba(231, 231, 235, 1)',
-      customYell: 'rgba(255, 236, 101, 1)'
-    },
-    fontFamily: {
-      raleway: ['Raleway', 'sans-serif'],
+    extend: {
+      colors: {
+        customDark: 'rgba(16, 14, 29, 1)',
+        customSecondaryDark: 'rgba(30, 33, 58, 1)',
+        customGray: 'rgba(97, 100, 117, 1)',
+        customgraytwo: 'rgba(110, 112, 122, 1)',
+        customtextW: 'rgba(231, 231, 235, 1)',
+        customYell: 'rgba(255, 236, 101, 1)'
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
     },
   },
   plugins: [],
